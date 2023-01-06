941b86098dd616b523c5b8664ec6a4011adbd2db
Wayne Piekarski <wayne@tinmith.net>
2023-01-05 20:03:55 -0800

    Extend all calculations to 28000 psi, add T/di=0.082", all calculations are linear unlike Stachiw's results which eventually break

--- stachiw/all_stachiw_multi.sh
+++ stachiw/all_stachiw_multi.sh
@@ -22,14 +22,16 @@ do_test() {
 
 for DIAMETER in "1.5"; do
     # Execute each t/Di test over the approximate range it has in Figure 7.12 before breaking
-    # The CalculiX results tend to be linear and not curved like in Stachiw's experiment
-    TDI="0.156"; for PSI in {0..2000..100};   do do_test "${TDI}" "${DIAMETER}" "${PSI}"; done
-    TDI="0.228"; for PSI in {0..4000..200};   do do_test "${TDI}" "${DIAMETER}" "${PSI}"; done
-    TDI="0.331"; for PSI in {0..8000..1000};  do do_test "${TDI}" "${DIAMETER}" "${PSI}"; done
-    TDI="0.407"; for PSI in {0..14000..1000}; do do_test "${TDI}" "${DIAMETER}" "${PSI}"; done
-    TDI="0.489"; for PSI in {0..20000..2000}; do do_test "${TDI}" "${DIAMETER}" "${PSI}"; done
-    TDI="0.559"; for PSI in {0..24000..2000}; do do_test "${TDI}" "${DIAMETER}" "${PSI}"; done
-    TDI="0.655"; for PSI in {0..24000..2000}; do do_test "${TDI}" "${DIAMETER}" "${PSI}"; done
+    # The CalculiX and equation results are perfectly linear and not curved like Stachiw's experiment, so fine resolution is not needed
+    # Generate one value at the maximum range of the graph so all the lines have the same length
+    TDI="0.082"; for PSI in {0..2000..100} 28000;   do do_test "${TDI}" "${DIAMETER}" "${PSI}"; done
+    TDI="0.156"; for PSI in {0..2000..100} 28000;   do do_test "${TDI}" "${DIAMETER}" "${PSI}"; done
+    TDI="0.228"; for PSI in {0..4000..200} 28000;   do do_test "${TDI}" "${DIAMETER}" "${PSI}"; done
+    TDI="0.331"; for PSI in {0..8000..1000} 28000;  do do_test "${TDI}" "${DIAMETER}" "${PSI}"; done
+    TDI="0.407"; for PSI in {0..14000..1000} 28000; do do_test "${TDI}" "${DIAMETER}" "${PSI}"; done
+    TDI="0.489"; for PSI in {0..20000..2000} 28000; do do_test "${TDI}" "${DIAMETER}" "${PSI}"; done
+    TDI="0.559"; for PSI in {0..24000..2000} 28000; do do_test "${TDI}" "${DIAMETER}" "${PSI}"; done
+    TDI="0.655"; for PSI in {0..24000..2000} 28000; do do_test "${TDI}" "${DIAMETER}" "${PSI}"; done
 # Execute everything in parallel, since only one line per process is written there should be no corruption due to race conditions
 done | time parallel --verbose
 
